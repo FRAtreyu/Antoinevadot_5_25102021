a48ae4de16be86ac6b839f64cc09c433b5a39dcf
Antoine Vadot <antoine.vadot@gmail.com>
2021-10-28 20:08:55 +0200

    index OK

--- front/js/script.js
+++ front/js/script.js
@@ -2,39 +2,27 @@ function getProducts(){
     return fetch("http://localhost:3000/api/products") .then((r)=>r.json());
 }
 
-function setProductLink(product){
-    document.getElementById("items").createElement("a");
-    document.querySelector("#items a").href="./product.html?id="+product._id;
-    document.querySelector("#items a").createElement("article");
-}
-
-function setProductImage(product){
-    let img = document.querySelector("#items a article").createElement("img");
-    img.href=product.imageUrl;
-    img.alt=product.altTxt;
-}
-
-function setProductName(product){
-    let h3 = document.querySelector("#items a article").createElement("h3");
-    h3.setAttribute("class","productName");
-    h3.innerText=product.name;
-}
-
-function setProductDescription(product){
-    let p = document.querySelector("#items a article").createElement("p");
-    p.setAttribute("class","productDescription");
-    p.innerText=product.description;
-
-}
-
-async function loadProducts(){
+async function displayProducts(){
     let products=await getProducts();
     for (let product of products) {
-        setProductLink(product);
-        setProductImage(product);
-        setProductName(product);
-        setProductDescription(product);
+        let a=document.createElement("a");
+        let article=document.createElement("article");
+        let img = document.createElement("img");
+        let h3 = document.createElement("h3");
+        let p = document.createElement("p");
+        a.href="./product.html?id="+product._id;
+        img.src=product.imageUrl;
+        img.alt=product.altTxt;
+        h3.setAttribute("class","productName");
+        h3.innerText=product.name;
+        p.setAttribute("class","productDescription");
+        p.innerText=product.description;
+        document.getElementById("items").appendChild(a);
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
     }
 }
 
-loadProducts();
+displayProducts();
