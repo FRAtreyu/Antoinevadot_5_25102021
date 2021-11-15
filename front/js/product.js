6a330273f79ae4e6b49ab4cefb5bf69e000ca0bf
Antoine Vadot <antoine.vadot@gmail.com>
2021-11-15 11:07:31 +0100

    final 1

--- front/js/product.js
+++ front/js/product.js
@@ -6,22 +6,27 @@ function getProduct() {//retourne le tableau avec les infos du produit de la pag
     return fetch(fetchURL).then((r) => r.json());
 }
 
-async function displayProduct() {//affiche les infos produit
-    let product = await getProduct();
-    let img = document.createElement("img");
-    img.src = product.imageUrl;
-    document.querySelector("div.item__img").appendChild(img);
-    document.getElementById("title").innerText = product.name;
-    document.getElementById("price").innerText = product.price;
-    document.getElementById("description").innerText = product.description;
-    let colors = product.colors;
-    for (let color of colors) {
-        let option = document.createElement("option");
-        option.value = color;
-        option.innerText = color;
-        document.getElementById("colors").appendChild(option);
+try {
+    async function displayProduct() {//affiche les infos produit
+        let product = await getProduct();
+        let img = document.createElement("img");
+        img.src = product.imageUrl;
+        document.querySelector("div.item__img").appendChild(img);
+        document.getElementById("title").innerText = product.name;
+        document.getElementById("price").innerText = product.price;
+        document.getElementById("description").innerText = product.description;
+        let colors = product.colors;
+        for (let color of colors) {
+            let option = document.createElement("option");
+            option.value = color;
+            option.innerText = color;
+            document.getElementById("colors").appendChild(option);
+        }
     }
+}catch (error){
+    console.log(error);
 }
+
 
 
 function selectValue() {//retourne la valeur selectionnée par l'user
@@ -29,22 +34,27 @@ function selectValue() {//retourne la valeur selectionnée par l'user
     return selectElement.options[selectElement.selectedIndex].value;
 }
 
-async function sendProduct() {//envoie les infos produit dans le localstorage pour le panier
-    let product = await getProduct();
-    let quantity = document.getElementById("quantity").value;
-    let productColor = selectValue();
-    let localStorageKey = product._id + productColor;
-    let productArray = [product._id, productColor, quantity]
-    if (!localStorage.getItem(localStorageKey) && Number(quantity) !== 0) {
-        localStorage.setItem(localStorageKey, JSON.stringify(productArray));
-    } else {
-        let array = JSON.parse(localStorage.getItem(localStorageKey));
-        let initQty = Number(array[2]);
-        array[2] = Number(quantity) + initQty;
-        localStorage.setItem(localStorageKey, JSON.stringify(array));
-    }
+try {
+    async function sendProduct() {//envoie les infos produit dans le localstorage pour le panier
+        let product = await getProduct();
+        let quantity = document.getElementById("quantity").value;
+        let productColor = selectValue();
+        let localStorageKey = product._id + productColor;
+        let productArray = [product._id, productColor, quantity]
+        if (!localStorage.getItem(localStorageKey) && Number(quantity) !== 0) {
+            localStorage.setItem(localStorageKey, JSON.stringify(productArray));
+        } else {
+            let array = JSON.parse(localStorage.getItem(localStorageKey));
+            let initQty = Number(array[2]);
+            array[2] = Number(quantity) + initQty;
+            localStorage.setItem(localStorageKey, JSON.stringify(array));
+        }
 
+    }
+} catch (error){
+    console.log(error);
 }
+
 
 displayProduct();
 document.getElementById("addToCart").addEventListener("click", sendProduct);
